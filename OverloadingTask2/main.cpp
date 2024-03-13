3c30a8ccdee449a532a70311f2bd054a29da43a9
flam1ngo <kris.s.03@mail.ru>
2024-03-13 11:18:08 +0300

    Initial Commit

---
+++ OverloadingTask2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OverloadingTask2
+//
+//  Created by Кристина Скворцова on 13.03.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
